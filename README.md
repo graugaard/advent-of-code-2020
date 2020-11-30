c3dc07c65dd5b776c1fcd355e73d4c2dd6f7ff32
graugaard <graugaard@users.noreply.github.com>
2020-11-30 20:54:37 +0100

    Minor changes README.md and have a simple unit test.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # advent-of-code-2020
 
-The solutions to advent of code 2020, written in Rust.
+The solutions to [advent of code 2020](https://adventofcode.com/2020), written in Rust.
--- src/day01.rs
+++ src/day01.rs
@@ -1,3 +1,11 @@
 pub fn season_greetings() {
 	println!("Merry christmas and happy holidays!");
 }
+
+#[cfg(test)]
+mod tests {
+		#[test]
+		fn basic_test() {
+				assert_eq!(2 + 2, 4);
+		}
+}
