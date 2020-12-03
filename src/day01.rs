2cb24778c98453ec305839155fd42c300b82ca21
graugaard <graugaard@users.noreply.github.com>
2020-12-03 21:37:52 +0100

    Optimized part 1 of day01

--- src/day01.rs
+++ src/day01.rs
@@ -2,22 +2,37 @@ use std::fs;
 
 pub fn print_solution() {
 		let input = puzzle_input();
-		let indexes = find_first_sum_to(&input, 2020);
-		println!("Day 01 Solution Part 1: {}", input[indexes.0] * input[indexes.1]);
+		let indexes = find_first_sum_to(&input, 2020).unwrap();
+		println!("Day 01 Solution Part 1: {}", indexes.0 * indexes.1);
 
 		let indexes = find_first_3_sum_to(&input, 2020);
 		println!("Day 01 Solution Part 2: {}", input[indexes.0] * input[indexes.1] * input[indexes.2]);
 }
 
-pub fn find_first_sum_to(values: &[i64], target: i64) -> (usize, usize) {
-		for i in 0..(values.len() - 1) {
-				for j in i + 1..values.len() {
-						if target == values[i] + values[j] {
-								return (i, j);
-						}
+pub fn find_first_sum_to(values: &[i64], target: i64) -> Option<(i64, i64)> {
+		let mut values = Vec::from(values);
+		values.sort();
+
+		find_summands_to_target(&values, target)
+}
+
+pub fn find_summands_to_target(values: &[i64], target: i64) -> Option<(i64, i64)> {
+		let mut lower = 0;
+		let mut upper = values.len() - 1;
+
+		while lower < upper {
+				let sum = values[lower] + values[upper];
+
+				if sum < target {
+						lower += 1;
+				} else if sum > target {
+						upper -= 1;
+				} else {
+						return Some((values[lower], values[upper]));
 				}
 		}
-		(0, 0)
+
+		None
 }
 
 
@@ -31,7 +46,7 @@ pub fn find_first_3_sum_to(values: &[i64], target: i64) -> (usize, usize, usize)
 						}
 				}
 		}
-		(0, 1, 2)
+		(0, 0, 0)
 }
 
 fn puzzle_input() -> Vec<i64> {
@@ -50,13 +65,13 @@ mod tests {
 		#[test]
 		fn values_1_2_3_and_target_2_gives_0_0() {
 				let values = [1, 2, 3];
-				assert_eq!(find_first_sum_to(&values, 2), (0, 0));
+				assert_eq!(find_first_sum_to(&values, 3), Some((1, 2)));
 		}
 
 		#[test]
 		fn values_1_4_5_and_target_9_gives_1_2() {
 				let values = [1, 4, 5];
-				assert_eq!(find_first_sum_to(&values, 9), (1, 2))
+				assert_eq!(find_first_sum_to(&values, 9), Some((4, 5)));
 		}
 
 		#[test]
