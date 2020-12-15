af3f1e27e00deceeaa64fd1b7abb0b5e5d820c6a
graugaard <graugaard@users.noreply.github.com>
2020-12-15 19:20:34 +0100

    Prettied the code if fmt command

--- src/day15.rs
+++ src/day15.rs
@@ -1,44 +1,44 @@
-use std::collections::HashMap;
 use crate::puzzle_input;
+use std::collections::HashMap;
 
 pub fn print_solution() {
     let input = puzzle_input::read_input("day15");
-    let input: Vec<usize> = input.split(",").map(str::parse::<usize>)
-      .map(Result::unwrap)
-      .collect();
+    let input: Vec<usize> = input
+        .split(",")
+        .map(str::parse::<usize>)
+        .map(Result::unwrap)
+        .collect();
 
     println!("Day 15 Solution Part 1: {}", n_th_number(&input, 2020));
     println!("Day 15 Solution Part 2: {}", n_th_number(&input, 30000000));
 }
 
 fn n_th_number(start: &[usize], turn: usize) -> usize {
-
     //let  turn = turn - 1; // it is specified we start at turn 1
     if turn <= start.len() {
-        return start[turn-1];
+        return start[turn - 1];
     }
 
     let mut val_to_round: HashMap<usize, usize> = HashMap::with_capacity(turn);
 
-    for (idx, &val) in start.iter().enumerate()
-    {
+    for (idx, &val) in start.iter().enumerate() {
         val_to_round.insert(val, idx + 1);
     }
 
     let mut last_number = *start.last().expect("Start is not empty");
-    for current_round in start.len() .. turn{
+    for current_round in start.len()..turn {
         match val_to_round.get(&last_number) {
             None => {
                 val_to_round.insert(last_number, current_round);
                 last_number = 0;
-            },
+            }
             Some(&number) => {
                 let tmp = current_round - number;
                 val_to_round.insert(last_number, current_round);
                 last_number = tmp;
             }
         }
-    };
+    }
 
     last_number
 }
@@ -99,13 +99,12 @@ mod tests {
 
     #[test]
     fn first_example() {
-        let start = [0,3,6];
+        let start = [0, 3, 6];
         assert_eq!(n_th_number(&start, 2020), 436);
     }
 
     #[test]
     fn second_example() {
-        assert_eq!(n_th_number(&[1,3,2], 2020), 1);
+        assert_eq!(n_th_number(&[1, 3, 2], 2020), 1);
     }
-
 }
